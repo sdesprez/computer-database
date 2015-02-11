dc942cc20b48ca99269b91b2b070530d792a3f2f
sdesprez <sdesprez@excilys.com>
2015-02-11 16:31:55 +0100

    Modularisation

---
+++ console/src/main/java/com/excilys/computerdatabase/cli/CLI.java
@@ -0,0 +1,351 @@
+package com.excilys.computerdatabase.cli;
+
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+import org.apache.commons.validator.GenericValidator;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import com.excilys.computerdatabase.domain.Company;
+import com.excilys.computerdatabase.domain.Computer;
+import com.excilys.computerdatabase.service.CompanyDBService;
+import com.excilys.computerdatabase.service.ComputerDBService;
+
+/**
+ * Command Line Interface
+ */
+public class CLI {
+
+	/**
+	 * Scanner for the inputs of the user
+	 */
+	private static Scanner sc;
+	/**
+	 * Instance of ComputerDBService for the access to the database
+	 */
+
+	private ComputerDBService computerDBService;
+	/**
+	 * Instance of CompanyDBService for the access to the database
+	 */
+
+	private CompanyDBService companyDBService;
+	
+	final ClassPathXmlApplicationContext context;
+
+	public CLI() {
+		context = new ClassPathXmlApplicationContext("service-context.xml");
+		computerDBService = (ComputerDBService) context.getBean("computerDBServiceImpl");
+		companyDBService = (CompanyDBService) context.getBean("companyDBServiceImpl");
+	}
+	
+	
+	/**
+	 * Main menu of the CLI
+	 */
+	public void mainMenu() {
+		//Instantiation of the scanner
+		sc = new Scanner(System.in);
+		
+		//Show the menu and ask the user for the function he wish to use as long as he wish to continue.
+		do {
+			System.out.println("0:Close");
+			System.out.println("1:List computers");
+			System.out.println("2:List companies");
+			System.out.println("3:Show computer details");
+			System.out.println("4:Add a computer");
+			System.out.println("5:Update a computer");
+			System.out.println("6:Delete a computer");
+			System.out.println("7:Delete a company (and all associated computers)");
+			
+			//Check the input and send to the corresponding function.
+			switch (sc.nextLine()) {
+			case "0":
+				sc.close();
+				return;
+			case "1":
+				listComputers();
+				break;
+			case "2":
+				listCompanies();
+				break;
+			case "3":
+				computerDetails();
+				break;
+			case "4":
+				createComputer();
+				break;
+			case "5":
+				updateComputer();
+				break;
+			case "6":
+				deleteComputer();
+				break;
+			case "7":
+				deleteCompany();
+				break;
+			default:
+				System.out.println("Invalid Input");
+				break;
+			}
+			
+			System.out.println("Do you wish to continue? (y/n)");
+			
+		} while (sc.nextLine().toLowerCase().equals("y"));
+		
+		context.close();
+		//Close the scanner
+		sc.close();
+	}
+	
+	/**
+	 * List the computers of the database by pages of 20 computers
+	 * You can navigate to the next page or the previous page
+	 */
+	public void listComputers() {		
+		final Pageable pageable = new PageRequest(0, 20);
+		//Get the first Page of computers from the database
+		Page<Computer> page = computerDBService.getPagedList("", pageable);
+		
+		//Show the content of the page
+		do {
+			System.out.println("Total : " + page.getTotalElements());
+			page.getContent().forEach(System.out::println);
+
+			System.out.println("e:Exit\np:Previous page\nn:Next page");
+			
+			switch(sc.nextLine().toLowerCase()) {
+			//Exit the listComputer() function
+			case "e":
+				return;
+			//Show the next Page
+			case "n":
+				if (page.hasNext()) {
+					page = computerDBService.getPagedList("", page.nextPageable());
+				}
+				System.out.println("Total : " + page.getTotalElements());
+				page.getContent().forEach(System.out::println);
+				break;
+			//Show the previous Page
+			case "p":
+				if (page.hasPrevious()) {
+					page = computerDBService.getPagedList("", page.previousPageable());
+				}
+				System.out.println("Total : " + page.getTotalElements());
+				page.getContent().forEach(System.out::println);
+				break;
+			default:
+				System.out.println("Invalid input");
+				break;
+			}
+		} while (true);
+	}
+	
+	/**
+	 * List all the companies of the database
+	 */
+	public void listCompanies() {
+		companyDBService.getAll().forEach(System.out::println);
+	}
+	
+	/**
+	 * Show the detail of a computer
+	 */
+	public void computerDetails() {
+		System.out.println("Enter the computer id");
+		
+		//Get the computer corresponding to the id
+		final Computer computer = computerDBService.getById(inputLong());
+		//Show the detail of the computer
+		if (computer != null) {
+			System.out.println("Name : " + computer.getName());
+			System.out.println("Introduced date : " + computer.getIntroduced());
+			System.out.println("Discontinued date : " + computer.getDiscontinued());
+			System.out.println("Company : " + computer.getCompany());
+		} else {
+			System.out.println("No computer found for this id");
+		}
+	}
+
+	/**
+	 * Interface for creating a new computer
+	 */
+	public void createComputer() {
+		final Computer.Builder builder = Computer.builder();
+		Company company = null;
+		
+		//Get the name of the computer, it can't be null or empty
+		System.out.println("Enter computer name");
+		builder.name(inputName());
+
+		//Get the date of introduction. If the user enter nothing, the date is null
+		System.out.println("Enter introduction date (yyyy-MM-dd) or nothing if you don't know");
+		builder.introduced(inputDate());
+		
+		//Get the date of discontinuation. If the user enter nothing, the date is null
+		System.out.println("Enter discontinued date (yyyy-MM-dd) or nothing if you don't know");
+		builder.discontinued(inputDate());
+
+		//Get the id of the company. If it's 0, then company = null
+		companyLabel : while (company == null) {
+			System.out.println("Enter company id (0 if you don't have one)");
+			final Long id = inputLong();
+			if (id == 0) {
+				break companyLabel;
+			}
+			company = companyDBService.getById(id);
+			if (company == null) {
+				System.out.println("This id doesn't correspond to any company in the database.");
+			}
+		}
+		builder.company(company);
+		
+		//Add the computer to the database
+		computerDBService.create(builder.build());
+		System.out.println("The computer was added to the database");
+		
+	}
+
+	/**
+	 * Interface for updating an existing computer
+	 */
+	public void updateComputer() {
+		System.out.println("Enter the id of the computer you wish to update");
+		//Get the computer to update from the database
+		final Computer computer = computerDBService.getById(inputLong());
+		Company company = null;
+		if (computer != null) {
+			//Change the name if the user wants
+			System.out.println("Current name : " + computer.getName());
+			System.out.println("Do you wish to change it?(y,n)");
+			if (sc.nextLine().toLowerCase().compareTo("y") == 0) {
+				System.out.println("Enter the new name");
+				computer.setName(inputName());
+			}
+			
+			//Change the introducedDate if the user wants
+			System.out.println("Current introduced date : " + computer.getIntroduced());
+			System.out.println("Do you wish to change it?(y,n)");
+			if (sc.nextLine().toLowerCase().compareTo("y") == 0) {
+				System.out.println("Enter the new introduced Date (yyyy-MM-dd)");
+				computer.setIntroduced(inputDate());
+			}
+			
+			//Change the discontinuedDate if the user wants
+			System.out.println("Current discontinued date : " + computer.getDiscontinued());
+			System.out.println("Do you wish to change it?(y,n)");
+			if (sc.nextLine().toLowerCase().compareTo("y") == 0) {
+				System.out.println("Enter the new dicontinued date (yyyy-MM-dd)");
+				computer.setDiscontinued(inputDate());
+			}
+			
+			//Change the company if the user wants
+			System.out.println("Current company id : " + computer.getCompany());
+			System.out.println("Do you wish to change it?(y,n)");
+			if (sc.nextLine().toLowerCase().compareTo("y") == 0) {
+				System.out.println("Enter the new company id (0 if you don't have one)");
+				companyLabel : while (company == null) {
+					System.out.println("Enter company id (0 if you don't have one)");
+					final Long id = inputLong();
+					if (id == 0) {
+						break companyLabel;
+					}
+					company = companyDBService.getById(id);
+					if (company == null) {
+						System.out.println("This id doesn't correspond to any company in the database.");
+					}
+				}
+				computer.setCompany(company);
+			}
+			
+			//Update the computer in the database
+			computerDBService.update(computer);
+			System.out.println("The computer was updated");
+		} else {
+			System.out.println("Incorrect id");
+		}
+	}
+	
+	/**
+	 * Interface for deleting a computer
+	 */
+	public void deleteComputer() {
+		Long id;
+		System.out.println("Enter the id of the computer you wish to delete");
+		id = inputLong();
+		computerDBService.delete(id);
+		System.out.println("The computer has been deleted");
+	}
+	
+	/**
+	 * Interface for deleting a company
+	 */
+	public void deleteCompany() {
+		Long id;
+		System.out.println("Enter the id of the company you wish to delete");
+		id = inputLong();
+		companyDBService.delete(id);
+		System.out.println("The company and it's computers have been deleted");
+	}
+	
+	/**
+	 * Function checking the user input to see if it's a valid computer name.
+	 * If the name isn't valid, it ask the user a new input until it's correct.
+	 * @return String : a valid computer name
+	 */
+	private String inputName() {
+		//Get the input
+		String name = sc.nextLine();
+		//Ask a new input while the name is empty or null
+		while (GenericValidator.isBlankOrNull(name)) {
+			System.out.println("Computer name can't be empty or spaces only");
+			System.out.println("Enter computer name");
+			name = sc.nextLine();
+		}
+		return name;
+	}
+	
+	/**
+	 * Function checking the user input to see if it's a valid Date name.
+	 * If the date isn't valid, it ask the user a new input until it's correct.
+	 * If the input is null or an empty String, return null;
+	 * @return LocalDate : a valid LocalDate 
+	 */
+	private LocalDate inputDate() {
+		String date = sc.nextLine();
+		//Check if the input is an empty chain or null
+		if (GenericValidator.isBlankOrNull(date)) {
+			return null;
+		}
+		while (!GenericValidator.isDate(date, "yyyy-MM-dd", false)) {
+			System.out.println("Incorrect date format");
+			System.out.println("Enter introduction date (yyyy-MM-dd) or nothing if you don't know");
+			date = sc.nextLine();
+			if (date == null || date.trim().isEmpty()) {
+				return null;
+			}
+		}
+		return LocalDate.parse(date, DateTimeFormatter.ISO_LOCAL_DATE);
+	}
+	
+	/**
+	 * Function checking the user input to see if it's a long.
+	 * If it isn't, the function ask a new input until it's valid.
+	 * @return long : a valid long
+	 */
+	private long inputLong() {
+		//Get the input
+		String stringLong = sc.nextLine();
+		//Check the input and ask a new one as long as the input isn't a long
+		while (!GenericValidator.isLong(stringLong)) {
+			System.out.println("Input is not a Long, enter a new input :");
+			stringLong = sc.nextLine();
+		}
+		return Long.valueOf(stringLong);
+	}
+}
---
+++ webapp/src/main/webapp/WEB-INF/views/dashboard.jsp
@@ -0,0 +1,97 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib tagdir="/WEB-INF/tags" prefix="h" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
+<jsp:include page="includes/header.jsp" />
+
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+<script>var alertString="<spring:message code='alert.confirm' javaScriptEscape='true'/>"</script>
+<script src="js/dashboard.js"></script>
+
+	
+	<section id="main">
+		<div class="container">
+			<span style="float: right"> 
+				<a href="?lang=en">en</a> 
+				| 
+				<a href="?lang=fr">fr</a>
+			</span>
+			<h1 id="homeTitle">${page.totalElements} <spring:message code="title.dashboard"/></h1>
+			<div id="actions" class="form-horizontal">
+				<div class="pull-left">
+					<form id="searchForm" action="dashboard" method="GET" class="form-inline">
+						<input type="hidden" name="size" value="${page.size}">
+						<input type="hidden" name="page" value="1">
+						<input type="search" id="searchbox" name="search"
+							class="form-control" placeholder="<spring:message code="placeholder.search"/>" /> <input
+							type="submit" id="searchsubmit" value="<spring:message code="button.filter"/>"
+							class="btn btn-primary" />
+					</form>
+				</div>
+				<div class="pull-right">
+					<a class="btn btn-success" id="add" href="add"><spring:message code="button.addComputer"/></a> 
+					<a class="btn btn-default" id="edit" href="#"	onclick="$.fn.toggleEditMode();"><spring:message code="button.edit"/></a>
+				</div>
+			</div>
+		</div>
+
+		<form id="deleteForm" action="delete" method="POST">
+			<input type="hidden" name="selection" value="">
+		</form>
+
+		<div class="container" style="margin-top: 10px;">
+			<table class="table table-striped table-bsorted">
+				<thead>
+					<tr>
+						<!-- Variable declarations for passing labels as parameters -->
+						<!-- Table header for Computer Name -->
+
+						<th class="editMode" style="width: 60px; height: 22px;"><input
+							type="checkbox" id="selectall" /> <span
+							style="vertical-align: top;"> - <a href="#"
+								id="deleteSelected" onclick="$.fn.deleteSelected();"> <i
+									class="fa fa-trash-o fa-lg"></i>
+							</a>
+						</span></th>
+					 	<%String[][] columns = {{"name", "column.name"}, {"introduced", "column.introduced"}, 
+								{"discontinued", "column.discontinued"}, {"company.name", "column.company"}}; 
+							pageContext.setAttribute("columns", columns);%>
+						<c:forEach items="${columns}" var="col">
+							<c:choose>
+							<c:when test="${col[0].equals(sort) && direction.equalsIgnoreCase(\"ASC\") }">
+								<th><h:link target="dashboard" size="${page.size}" search="${search}" sort="${col[0]}" direction="desc"><spring:message code="${col[1]}"/></h:link></th>
+							</c:when>
+							<c:otherwise>
+								<th><h:link target="dashboard" size="${page.size}" search="${search}" sort="${col[0]}" direction="asc"><spring:message code="${col[1]}"/></h:link></th>
+							</c:otherwise>
+							</c:choose>
+						</c:forEach>				
+						</tr>
+				</thead>
+				<!-- Browse attribute computers -->
+				<tbody id="results">
+				<c:forEach items="${page.content}" var="computer">
+					<tr>
+						<td class="editMode"><input type="checkbox" name="cb" class="cb" value="${computer.id}"></td>
+						<td><a href="edit?id=${computer.id}"><c:out value="${computer.name}"/> </a></td>
+						<td>${computer.introduced}</td>
+						<td>${computer.discontinued}</td>
+						<td>${computer.company.name}</td>
+					</tr>
+				</c:forEach>
+				</tbody>
+			</table>
+		</div>
+	</section>
+
+	<footer class="navbar-fixed-bottom">
+		<div class="container text-center">
+			<h:pagination target="dashboard" pages="${page.totalPages}" pageNumber="${page.number}" size="${page.size}" search="${search}" sort="${sort}" direction="${direction}"/>
+		</div>
+	</footer>
+</body>
+</html>
